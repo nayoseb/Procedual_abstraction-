e719fc1bd436a3fc2beef417adb869c66a1df9df
nayoseb <nayoseb1018@gmail.com>
2023-11-16 10:04:40 +0900

    :recycle: Experiments 작성

--- com/tip/functional/Experiments.java
+++ com/tip/functional/Experiments.java
@@ -3,5 +3,41 @@ package com.tip.functional;
 import java.util.Iterator;
 
 public class Experiments<T extends Number> implements Iterator<T> {
-    // TODO: 채우기
+    private final Iterator<T> internalIterator; // 내부 반복자
+    private final String herbAvailabilities; // 이름
+    private final String distributionDescription; // 분포 설명
+    private int count = 0; // 계수
+    private double sum = 0; // 합계
+
+    public Experiments(Iterator<T> internalIterator, String herbAvailabilities, String distributionDescription) {
+        this.internalIterator = internalIterator;
+        this.herbAvailabilities = herbAvailabilities;
+        this.distributionDescription = distributionDescription;
+    }
+
+    @Override
+    public boolean hasNext() {
+        // 래핑된 반복자가 더 많은 요소가 있는지 여부를 결정합니다.
+        return true;
+    }
+
+    @Override
+    public T next() {
+        // 반복자가 제공하는 값의 계수와 합계를 추적하려고 합니다.
+        T value = internalIterator.next();
+        count++;
+        sum += value.doubleValue();
+        return value;
+    }
+
+    public void report() {
+        // 분포 설명과 처리된 요소의 계수, 평균값을 출력하는 간단한 보고 메서드입니다.
+        System.out.println(herbAvailabilities + " - " + distributionDescription);
+        System.out.println("계수: " + count);
+        System.out.println("합계: " + sum);
+        if (count > 0) {
+            System.out.println("평균: " + (sum / count));
+        }
+    }
+
 }
