4c97cf23db7848cdcb7c6845235f1a18abcaed74
nayoseb <nayoseb1018@gmail.com>
2023-11-16 10:02:43 +0900

    :recycle: Fibonacci 작성

--- com/tip/functional/Fibonacci.java
+++ com/tip/functional/Fibonacci.java
@@ -2,4 +2,25 @@ package com.tip.functional;
 
 public class Fibonacci implements InfiniteIterator<Integer> {
     // TODO: 채우기
+    private int current = 1;
+    private int next = 1;
+
+    public int getCurrent() {
+        return current;
+    }
+
+    @Override
+    public boolean hasNext() {
+        // 피보나치 수열은 무한하므로 항상 true를 반환합니다.
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        int result = current;
+        int newNext = current + next;
+        current = next;
+        next = newNext;
+        return result;
+    }
 }
